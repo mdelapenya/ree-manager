0c7d0aa1d7b151041a2d20e70a4591a25cd8701b
Manuel de la Peña <manuel.delapenya@liferay.com>
2014-04-06 19:20:44 +0200

    Not sure how this was rollbacked

--- src/main/java/es/gme/ree/manager/url/ReeURLBuilderImpl.java
+++ src/main/java/es/gme/ree/manager/url/ReeURLBuilderImpl.java
@@ -7,13 +7,25 @@ package es.gme.ree.manager.url;
  */
 public class ReeURLBuilderImpl implements ReeURLBuilder {
 
+	public ReeURLBuilderImpl() {
+		this(new ReeURLContext());
+	}
+
+	public ReeURLBuilderImpl(int month, int year) {
+		this(new ReeURLContext(month, year));
+	}
+
+	public ReeURLBuilderImpl(ReeURLContext reeURLContext) {
+		_reeURLContext = reeURLContext;
+	}
+
 	public String getURL(int month, int year) throws IllegalArgumentException {
 		if (month < 1 || month > 12) {
 			throw new IllegalArgumentException("An invalid month has been introduced.");
 		}
 
 		String fullUrl =
-			_getWebURL() + _getApplicationURL() + _getFileName(month, year) + _getFileType() + _getLanguage() +
+			_getWebURL() + _getApplicationURL() + _getFileName() + _getFileType() + _getLanguage() +
 			_getRequestType();
 
 		return fullUrl;
@@ -23,8 +35,10 @@ public class ReeURLBuilderImpl implements ReeURLBuilder {
 		return "Solicitar";
 	}
 
-	private String _getFileName(int month, int year) {
+	private String _getFileName() {
 		String strMonth = "";
+
+		int month = _reeURLContext.getMonth();
 
 		if (month < 10) {
 			strMonth += "0";
@@ -32,23 +46,36 @@ public class ReeURLBuilderImpl implements ReeURLBuilder {
 
 		strMonth += month;
 
-		return "?fileName=C3_liquicomun_" + year + strMonth;
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("?fileName=");
+		sb.append("C");
+		sb.append(_reeURLContext.getcType());
+		sb.append("_");
+		sb.append(_reeURLContext.getFileName());
+		sb.append("_");
+		sb.append(_reeURLContext.getYear());
+		sb.append(strMonth);
+
+		return sb.toString();
 	}
 
 	private String _getFileType() {
-		return "&fileType=zip";
+		return "&fileType=" + _reeURLContext.getFileType();
 	}
 
 	private String _getLanguage() {
-		return "&idioma=es";
+		return "&idioma=" + _reeURLContext.getLanguage();
 	}
 
 	private String _getRequestType() {
-		return "&tipoSolicitar=Publicaciones";
+		return "&tipoSolicitar=" + _reeURLContext.getRequestType();
 	}
 
 	private String _getWebURL() {
 		return "http://www.esios.ree.es/";
 	}
+
+	private ReeURLContext _reeURLContext;
 
 }
